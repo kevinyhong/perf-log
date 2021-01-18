eae46b0bdba59f141cab7c10b0b942450c067ae1
kevinyhong <kevinyaohuiong@gmail.com>
2021-01-18 01:41:32 -0800

    Updated package.json with exports field

--- perf-log.js
+++ perf-log.js
@@ -3,7 +3,7 @@ const { performance } = require('perf_hooks')
 module.exports = (req, res, next) => {
   req.requestTime = performance.now();
   req.timeout(60000, () => {
-    res.status(408).end(JSON.stringify(new Error("Client request timed out after 60 seconds. Please verify that all inputs for the request are correct.")));
+    res.status(408).end(JSON.stringify(new Error("Client request timed out after 60 seconds.")));
   })
   next();
 }
